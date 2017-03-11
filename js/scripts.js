5334bf5e78c82b393a8394b0d89dc82330595124
Emily Dokken <dokkenemily@gmail.com>
2017-03-11 10:38:51 -0800

    add backend logic and create branchEquation function to run with input values in frontend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
 // Back End Logic
 
+var branchEquation = function(number) {
 
+  if (number % 15 === 0) {
+    $("#list").append('<li>' + "ping" + '</li>');
+  } else if (number % 5 === 0) {
+      $("#list").append('<li>' + "Pong" + '</li>');
+  } else if (number % 3 === 0) {
+      $("#list").append('<li>' + "pingPong" + '</li>');
+  } else {
+      $("#list").append('<li>' + number + '</li>');
+    }
+
+};
 
 // Front End Logic
 
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var number = parseInt($("#number").val());
-
-    if (number % 15 === 0) {
-      $("#left").append('<li>' + "ping" + '</li>');
-    }
-      else if (number % 5 === 0) {
-        $("#right").append('<li>' + "Pong" + '</li>');
-    }
-      else if (number % 3 === 0) {
-        $("#left").append('<li>' + "pingPong" + '</li>');
-    }
-      else {
-        $("#netCenter").append('<li>' + number + '</li>');
-      }
+    var input = parseInt($("#number").val());
+    var result = branchEquation(input);
+    console.log(result);
   });
 });
