69dbff4a4f1c5656e208f4b4a85b6cfa686fa66e
Emily Dokken <dokkenemily@gmail.com>
2017-03-22 22:52:22 -0700

    add javascript to clear form on submit, adjust pingpong function so that the correct words show up based on which numbers are entered

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,11 @@ var pingPong = function(number) {
   var pingPongArray = [];
   for (var i = 1; i <= number; i += 1) {
     if (i % 15 === 0) {
-      pingPongArray.push("Ping");
+      pingPongArray.push("PingPong");
     } else if (i % 5 === 0) {
       pingPongArray.push("Pong");;
     } else if (i % 3 === 0) {
-      pingPongArray.push("PingPong");;
+      pingPongArray.push("Ping");;
     } else {
       pingPongArray.push(i);;
       }
@@ -20,9 +20,10 @@ var pingPong = function(number) {
 // Front End Logic
 
 $(document).ready(function() {
+
   $("form").submit(function(event) {
     event.preventDefault();
-
+    $("ul#list").empty();
     var input = parseInt($("#number").val());
 
     if (isNaN(input)) {
@@ -34,7 +35,6 @@ $(document).ready(function() {
       result.forEach(function(result) {
         $("#list").append('<li>' + result  + '</li>');
       });
-
 
     });
 });
